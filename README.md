1f66488e5ea274306196e97a91407e20884bed45
sombriks <sombriks@gmail.com>
2019-06-24 18:56:23 -0300

    sample1 working but presenting bug on firefox mobile

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# sample drag drop vue
+# Sample drag drop vue
 
 Small experiments to tease drag drop solutions with vue
 
-## libraries being evaluated
+## Libraries being evaluated
 
 - [vue-drag-drop](https://github.com/cameronhimself/vue-drag-drop)
 - [Vue.Draggable](https://github.com/SortableJS/Vue.Draggable)
 - [vue-smooth-dnd](https://github.com/kutlugsahin/vue-smooth-dnd)
+
+## Key characteristic being tested
+
+Once drop event fires, we must detect drop target and instead to shift or
+anything like that we need to make drag source a children of drop target.
--- sample1/Home.vue
+++ sample1/Home.vue
@@ -1,13 +1,54 @@
 <template>
-    
+    <div>
+        <h1>Drag drop with vue-drag-drop</h1>
+        <button v-if="current.pai" @click="current = current.pai">Voltar</button> 
+        <div class="elementos" v-for="c in current.children" :key="c.id">
+            <drop @drop="drop => handleDrop(drop,c)">
+                <drag :transfer-data="c">
+                    <div>{{c.val}}</div> <button @click="current = c">...</button>
+                </drag> 
+            </drop>
+        </div>
+    </div>
 </template>
 
 <script>
+const { Drag, Drop } = require("vue-drag-drop");
 module.exports = {
-    name:"Home"
+    name:"Home",
+    components: { Drag, Drop },
+    data() {
+        return {
+            root:{i:-1, val:"Root", children:[]},
+            current:null
+        }
+    },
+    created() {
+        this.current=this.root;
+        let i = 10;
+        while(i-->0) {
+            this.current.children.push({ 
+                pai:this.root, 
+                val:`item ${i}`, 
+                children:[],
+                id:i, 
+            })
+        }
+    },
+    methods: {
+        handleDrop(drop, target) {
+            drop.pai.children = drop.pai.children.filter(e => e != drop);
+            drop.pai = target;
+            target.children.push(drop);
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+    .elementos {
+        margin: 1em 1em 1em 1em;
+        padding: 1em 1em 1em 1em;
+        border: 1px solid black;
+    }
 </style>
